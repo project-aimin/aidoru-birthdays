a5db02fca8e9e655e8624edbc81fba133a0957b2
Yukina <themunyang21@naver.com>
2020-05-22 00:30:15 +0900

    :)

---
+++ js/common.js
@@ -0,0 +1,10 @@
+function toggleVisiblities(id_element,display_type){
+ var eTarget = document.getElementById(id_element);
+ if(eTarget.style.display != "none"){eTarget.style.display = "none";}
+ else{eTarget.style.display = display_type;}
+}
+
+function openMailAddr(elem){
+ var mailaddr = elem.dataset.name +"@"+ elem.dataset.domain +"."+ elem.dataset.tld;
+ window.location.href = "mailto:"+ mailaddr;
+}
