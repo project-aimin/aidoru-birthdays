9ab797be0f8c1d26b0ad448a3af58dfa6b05cdea
Yukina <themunyang21@naver.com>
2020-05-22 00:32:35 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -7,4 +7,5 @@ function toggleVisiblities(id_element,display_type){
 function openMailAddr(elem){
  var mailaddr = elem.dataset.name +"@"+ elem.dataset.domain +"."+ elem.dataset.tld;
  window.location.href = "mailto:"+ mailaddr;
+ return;
 }
