0d72d494f8cb27c548e212ff49b3c508b04e5389
Yukina <themunyang21@naver.com>
2020-05-27 09:53:59 +0900

    Update render_table.js

--- js/render_table.js
+++ js/render_table.js
@@ -2,32 +2,31 @@ var renderTable = {
  run : function(jsonFilePath,tableID,insertTo){
   var fetchFile = new XMLHttpRequest();
   fetchFile.onreadystatechange = function(){
-   var rawData = JSON.parse(fetchFile.responseText);
-   var rowTitle = rawData.rows;
-
-   var wholeTable = document.createElement("table");
-   wholeTable.id = tableID;
-
-   var tableHeader = document.createElement("tr");
-   for(r=0;r<rowTitle.length;r++){
-    var rowsEach = document.createElement("th");
-    rowsEach.appendChild(document.createTextNode(rowTitle[r]));
-    tableHeader.appendChild(rowsEach);
-   }
-
-   wholeTable.appendChild(tableHeader);
-
-   var entriesData = rawData.entries;
-   for(e=0;e<entriesData.length;e++){
-    var entriesEach = document.createElement("tr");
-    for(d=0;d<rowTitle.length;d++){
-     var datasEach = document.createElement("td");
-     datasEach.appendChild(document.createTextNode(entriesData[e][d]));
-     entriesEach.appendChild(datasEach);
+   if(this.readyState == 4 && this.status == 200){
+    var rawData = JSON.parse(fetchFile.responseText);
+    var rowTitle = rawData.rows;
+    var wholeTable = document.createElement("table");
+    wholeTable.id = tableID;
+    var tableHeader = document.createElement("tr");
+    for(r=0;r<rowTitle.length;r++){
+     var rowsEach = document.createElement("th");
+     rowsEach.appendChild(document.createTextNode(rowTitle[r]));
+     tableHeader.appendChild(rowsEach);
     }
-    wholeTable.appendChild(entriesEach);
+    
+    wholeTable.appendChild(tableHeader);
+    var entriesData = rawData.entries;
+    for(e=0;e<entriesData.length;e++){
+     var entriesEach = document.createElement("tr");
+     for(d=0;d<rowTitle.length;d++){
+      var datasEach = document.createElement("td");
+      datasEach.appendChild(document.createTextNode(entriesData[e][d]));
+      entriesEach.appendChild(datasEach);
+     }
+     wholeTable.appendChild(entriesEach);
+    }
+    document.getElementById(insertTo).appendChild(wholeTable);
    }
-   document.getElementById(insertTo).appendChild(wholeTable);
   };
   fetchFile.open("GET",jsonFilePath);
   fetchFile.send();
