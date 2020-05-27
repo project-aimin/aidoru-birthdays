e305a24f06dc660bde9f29857b6fd2f2adde4cb6
Yukina <themunyang21@naver.com>
2020-05-27 09:25:42 +0900

    dynamic table

---
+++ js/render_table.js
@@ -0,0 +1,32 @@
+
+
+var renderTable = {
+ run = function(jsonFilePath,tableID,insertTo){
+  var fetchFile = new XMLHttpRequest();
+  fetchFile.onreadystatechange = function(){
+   var rawData = JSON.parse(fetchFile.responseText);
+   var rowTitle = rawData.rows;
+
+   var wholeTable = document.createElement("table");
+   wholeTable.id = tableID;
+   
+   var tableHeader = document.createElement("tr");
+   for(r=0;r<rowTitle.length;r++){
+    var rowsEach = document.createElement("th");
+    rowsEach.appendChild(document.createTextNode(rowTitle[r]));
+   }
+
+   var entriesData = rawData.entries;
+   for(e=0;e<entriesData.length;e++){
+    var entriesEach = document.createElement("tr");
+    for(d=0;d<rowTitle.length;d++){
+     var datasEach = document.createElement("td");
+     datasEach.appendChild(document.createTextNode(entriesData[e][d]));
+     entriesEach.appendChild(datasEach);
+    }
+    wholeTable.appendChild(entriesEach);
+   }
+   document.getElementById(insertTo).appendChild(wholeTable);
+  };
+ }
+};
