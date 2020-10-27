1c6551b3500e5ead7dd3fa6033cc62f0e2183c10
Yukina <themunyang21@naver.com>
2020-10-27 19:10:10 +0900

    Update render_table.js

--- js/render_table.js
+++ js/render_table.js
@@ -61,10 +61,10 @@ var renderTable = {
   var day = (today.getDate());
   var md = mon+"/"+day;
 
-  $(`#birthday_table`).height() - $(`#birthday_table`).outerHeight()) / 2)
+   - $(`#birthday_table`).outerHeight()) / 2)
 
   var charasToday = $(`tr[data-calendar-day='${md}']`);
-  var scrollToday = charasToday.position().top;
+  var scrollToday = charasToday.position().top + charasToday.height();
   
   $(`#${tableTo}`).animate({scrollTop : scrollToday},100);
  }
