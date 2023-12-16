cd80dfea182d5f5714a858a33606b9daf3cb9a75
Rina Hayasaka <themunyang21@naver.com>
2023-12-16 10:40:55 +0900

    .

--- js/render_table.js
+++ js/render_table.js
@@ -34,6 +34,7 @@ var renderTable = {
 
      var monEntryCur = new Number(entriesData[e][0].split("/")[0]);
      var monEntryNext = new Number(entriesData[e+1][0].split("/")[0]);
+     console.log(e);
      if(monEntryCur != monEntryNext){
       var monthsEach = document.createElement("th");
       var monthLine = document.createElement("td");
